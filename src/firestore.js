b28fe3416c18cfd1e804ab77c29df9f9e9c96ffc
vinhbui999 <vinhbui268@gmail.com>
2021-07-05 20:46:03 +0700

    final commit

--- src/firestore.js
+++ src/firestore.js
@@ -1,14 +1,14 @@
-import firebase from 'firebase';
+import firebase from "firebase";
 const firebaseConfig = {
-    apiKey: "AIzaSyDOC5PzR3vbmmgNwCjWNrp7pYX2gz0Vdw8",
-    authDomain: "signintest-53945.firebaseapp.com",
-    projectId: "signintest-53945",
-    storageBucket: "signintest-53945.appspot.com",
-    messagingSenderId: "836942836631",
-    appId: "1:836942836631:web:f7621132943e95f36e4ad2",
-    measurementId: "G-JJE6DV0SY1",
-    databaseURL: "https://signintest-53945-default-rtdb.firebaseio.com/"
-  };
+  apiKey: "AIzaSyDOC5PzR3vbmmgNwCjWNrp7pYX2gz0Vdw8",
+  authDomain: "signintest-53945.firebaseapp.com",
+  databaseURL: "https://signintest-53945-default-rtdb.firebaseio.com",
+  projectId: "signintest-53945",
+  storageBucket: "signintest-53945.appspot.com",
+  messagingSenderId: "836942836631",
+  appId: "1:836942836631:web:f7621132943e95f36e4ad2",
+  measurementId: "G-JJE6DV0SY1"
+};
 
 firebase.initializeApp(firebaseConfig);
 
